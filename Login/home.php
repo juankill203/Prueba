88eebb042b058abe4942c2e6a20ba4bd0b79b03f
juan david <52943310+juankill203@users.noreply.github.com>
2021-11-12 21:12:15 -0500

    Add files via upload

---
+++ Login/home.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Entrada</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+     <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
+     <a href="Salida.php">Logout</a>
+</body>
+</html>
+
+<?php 
+}else{
+     header("Location: Lp.php");
+     exit();
+}
+ ?>
