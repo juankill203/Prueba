70a7a31f4cba26e9874b48099848cbe3d5de67b2
juan david <52943310+juankill203@users.noreply.github.com>
2021-11-12 20:41:12 -0500

    Update Trabajo Juan perez.sql

--- Tarea Repositorio/Trabajo Juan perez.sql
+++ Tarea Repositorio/Trabajo Juan perez.sql
@@ -544,8 +544,7 @@ Id_rol int,
 Id_tiposact int, 
 correo varchar(30), 
 foreign key (Id_generos) references generos(Id_generos), 
-foreign key (Id_tiposact) references tiposact(Id_tiposact), 
-foreign key (Id_otros) references otros(Id_otros), 
+foreign key (Id_login) references otros(Id_otros), 
 foreign key (Id_estados) references estados(Id_estados), 
 foreign key (Id_rol) references rol(Id_rol));
 
@@ -554,9 +553,9 @@ desc usuarios
 
 create table rol( 
 Id_rol int, 
-Cliente int, 
-Administrador int, 
-Proovedor int, 
+Cliente String, 
+Administrador String, 
+Proovedor String, 
  
 constraint P_ico primary key(Id_rol));
 
@@ -587,39 +586,6 @@ values('Juan','JuanitoAlcachofa','alqueria')
  
 desc rol;
 
-create table tiposact( 
-Id_tiposact int, 
-SoporteTecnico varchar(10), 
-actualizacion varchar(10), 
-MantenimientoSoftware  varchar(10), 
-MantenimientoHardware varchar(10), 
-constraint P_ico3 primary key(Id_tiposact));
-
-desc tiposact
-
-
-create table otros( 
-Id_otros int, 
-Microfonos int, 
-CamaraWeb int, 
-Joystick int, 
-teclados int, 
-mause int, 
-constraint P_ico4 primary key(Id_otros));
-
-desc otros
-
-
-create table estados( 
-Id_estados int, 
-Funcional varchar(10), 
-Nofuncional varchar(10), 
-FuncionalConErrores varchar(10), 
-constraint P_ico5 primary key(Id_estados));
-
-desc estados
-
-
 create table usuarios( 
 Id_usuarios int, 
 cedula int, 
@@ -638,132 +604,18 @@ foreign key (Id_rol) references rol(Id_rol));
 desc usuarios
 
 
-create table rol( 
-Id_rol int, 
-Cliente int, 
-Administrador int, 
-Proovedor int, 
+create table login( 
+Cliente String, 
+Contraseña String,
  
-constraint P_ico primary key(Id_rol));
+constraint P_ico primary key(Id_login));
 
-insert into rol(Id_rol, Cliente, Administrador, Proovedor) 
-values(1,'Juan','JuanitoAlcachofa','alqueria') 
+insert into login(Cliente, Contraseña) 
+values('Juan','juan') 
  
-desc rol;
+desc login;
 
-create table rol( 
-Id_rol int, 
-Cliente varchar(20), 
-Administrador varchar(20), 
-Proovedor varchar(20), 
+
  
-constraint P_ico primary key(Id_rol));
 
-insert into rol(Id_rol, Cliente, Administrador, Proovedor) 
-values(1,'Juan','JuanitoAlcachofa','alqueria') 
- 
-desc rol;
-
-create table rol( 
-Id_rol int, 
-Cliente varchar(20), 
-Administrador varchar(20), 
-Proovedor varchar(20), 
- 
-constraint P_ico primary key(Id_rol));
-
-insert into rol(Id_rol, Cliente, Administrador, Proovedor) 
-values(1,'Juan','JuanitoAlcachofa','alqueria') 
- 
-desc rol;
-
-create table rol( 
-Id_rol int, 
-Cliente varchar(30), 
-Administrador varchar(30), 
-Proovedor varchar(30), 
- 
-constraint P_ico primary key(Id_rol));
-
-insert into rol(Id_rol, Cliente, Administrador, Proovedor) 
-values(1,'Juan','JuanitoAlcachofa','alqueria') 
- 
-desc rol;
-
-create table otros( 
-Id_otros int, 
-Microfonos int, 
-CamaraWeb int, 
-Joystick int, 
-teclados int, 
-mause int, 
-constraint P_ico4 primary key(Id_otros));
-
-insert into otros(Id_otros, Microfonos, CamaraWeb, Joystick, teclados, mause) 
-values(2, 8, 9, 6, 3);
-
-create table otros( 
-Id_otros int, 
-Microfonos int, 
-CamaraWeb int, 
-Joystick int, 
-teclados int, 
-mause int, 
-constraint P_ico4 primary key(Id_otros));
-
-insert into otros(Id_otros, Microfonos, CamaraWeb, Joystick, teclados, mause) 
-values(2, 8, 9, 6, 3, 2);
-
-create table otros( 
-Id_otros int, 
-Microfonos int, 
-CamaraWeb int, 
-Joystick int, 
-teclados int, 
-mause int, 
-constraint P_ico4 primary key(Id_otros));
-
-insert into otros(Id_otros, Microfonos, CamaraWeb, Joystick, teclados, mause) 
-values(2, 8, 9, 6, 3, 2) 
- 
- 
-desc otros;
-
-create table otros( 
-Id_otros int, 
-Microfonos int, 
-CamaraWeb int, 
-Joystick int, 
-teclados int, 
-mause int, 
-constraint P_ico4 primary key(Id_otros));
-
-insert into otros(Id_otros, Microfonos, CamaraWeb, Joystick, teclados, mause) 
-values(2, 8, 9, 6, 3, 2) 
-insert into otros values(1, 1, 1, 2, 6, 5) 
- 
-desc otros;
-
-insert into otros values(1, 1, 1, 2, 6, 5);
-
-insert into otros(Id_otros, Microfonos, CamaraWeb, Joystick, teclados, mause) 
-values(2, 8, 9, 6, 3, 2) 
-insert into otros values(1, 1, 1, 2, 6, 5);
-
-values(2, 8, 9, 6, 3, 2)
-
-
-insert into otros(Id_otros, Microfonos, CamaraWeb, Joystick, teclados, mause);
-
-create table estados( 
-Id_estados int, 
-Funcional varchar(10), 
-Nofuncional varchar(10), 
-FuncionalConErrores varchar(10), 
-constraint P_ico5 primary key(Id_estados));
-
-insert into estados(Id_estados, funcional, Nofuncional, FuncionalConerrores) 
-values(2,'no','si','no') 
- 
-desc estados;
 
